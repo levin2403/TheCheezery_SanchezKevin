a90de76afcafaf255e0d19ca38f788fec7bbc09d
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2025-06-11 23:39:04 -0700

    Primer intento
    Primer version abr si si quedo

--- app/src/main/java/sanchez/kevin/thecheezery/MenuActivity.kt
+++ app/src/main/java/sanchez/kevin/thecheezery/MenuActivity.kt
@@ -1,20 +1,44 @@
 package sanchez.kevin.thecheezery
 
-import android.os.Bundle
+
 import androidx.activity.enableEdgeToEdge
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var btnCold: Button = findViewById<Button>(R.id.button_cold_drinks)
+        var btnHot: Button = findViewById<Button>(R.id.button_hot_drinks)
+        var btnSweets: Button = findViewById<Button>(R.id.button_sweets)
+        var btnSalties: Button = findViewById<Button>(R.id.button_salties)
+
+
+        btnCold.setOnClickListener{
+            var intent: Intent = Intent(this,ProductsActivity::class.java)
+            intent.putExtra("menuType","coldDrinks")
+            startActivity(intent)
+        }
+        btnHot.setOnClickListener{
+            var intent: Intent = Intent(this,ProductsActivity::class.java)
+            intent.putExtra("menuType","hotDrinks")
+            startActivity(intent)
+        }
+        btnSweets.setOnClickListener{
+            var intent: Intent = Intent(this,ProductsActivity::class.java)
+            intent.putExtra("menuType","sweets")
+            startActivity(intent)
+        }
+        btnSalties.setOnClickListener{
+            var intent: Intent = Intent(this,ProductsActivity::class.java)
+            intent.putExtra("menuType","salties")
+            startActivity(intent)
         }
     }
 }
+
---
+++ app/src/main/java/sanchez/kevin/thecheezery/Product.kt
@@ -0,0 +1,8 @@
+package sanchez.kevin.thecheezery
+
+data class Product(var name: String,
+                   var image: Int,
+                   var descripcion: String,
+                   var price: Double) {
+
+}
